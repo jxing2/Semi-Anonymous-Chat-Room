093396c2daba54862fd7a4f2e7566d5bbadbf267
OnigiriFan <fanxiaoyu321@gmail.com>
2015-12-05 21:08:06 -0500

    download: if a file with the same name exists in selected directory, pop up a dialog

--- Semi-Anonymous_Chat_Room/src/Client/DownloadWindow.java
+++ Semi-Anonymous_Chat_Room/src/Client/DownloadWindow.java
@@ -12,7 +12,7 @@ import java.awt.event.ActionListener;
 import java.io.File;
 
 public class DownloadWindow extends JFrame {
-	//private File fileDir;
+	// private File fileDir;
 	private JButton download;
 	private JButton cancel;
 	private JScrollPane jsp;
@@ -22,22 +22,24 @@ public class DownloadWindow extends JFrame {
 	public String getSavePath() {
 		return savePath;
 	}
+
 	private String fileToSave;
-	private int upperMargin=20;
+	private int upperMargin = 20;
 	private int interval = 20;
-	private int leftMargin=30;
+	private int leftMargin = 30;
 	private int width = 340;
-	private int sizeX = 400, sizeY = 424;//window size
+	private int sizeX = 400, sizeY = 424;// window size
 	private JTree tree;
 	private File folder;
 	DefaultMutableTreeNode selectedNode;
 	DefaultMutableTreeNode top = new DefaultMutableTreeNode("Share");
+	JFileChooser fileChooser;
 	public DownloadWindow(final Client parent) {
-		//tree = new TreeViewer(fileDir);
+		// tree = new TreeViewer(fileDir);
 		this.parent = parent;
-//		for (int i = 0; i < listOfFiles.length; i++) {
-//			top.add(new DefaultMutableTreeNode(listOfFiles[i].getName()));
-//		}
+		// for (int i = 0; i < listOfFiles.length; i++) {
+		// top.add(new DefaultMutableTreeNode(listOfFiles[i].getName()));
+		// }
 		c = getContentPane();
 		c.setLayout(null);
 		tree = new JTree(top);
@@ -46,67 +48,69 @@ public class DownloadWindow extends JFrame {
 		download = new JButton("Download");
 		download.setBounds(leftMargin, upperMargin += interval + 300, 120, 30);
 		cancel = new JButton("Cancel");
-		cancel.setBounds(sizeX - leftMargin - 120 , upperMargin, 120, 30);
+		cancel.setBounds(sizeX - leftMargin - 120, upperMargin, 120, 30);
 
 		c.add(download);
 		c.add(cancel);
 		c.add(jsp);
-		//c.add(tree);
-		setSize(sizeX,sizeY);
+		// c.add(tree);
+		setSize(sizeX, sizeY);
 		setResizable(false);
 		Client.centreWindow(this);
 		setVisible(true);
-		//System.out.println("Download");
-		//JTree tree = new JTree (addNodes(null,fileDir)) ;
-		
-		download.addActionListener(//send file upload request
-				new ActionListener(){
-					public void actionPerformed(ActionEvent e){
-//						if (fileName_txt.getText().trim().equals("")){
-//							return;
-//						}
+		// System.out.println("Download");
+		// JTree tree = new JTree (addNodes(null,fileDir)) ;
+
+		download.addActionListener(// send file upload request
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						// if (fileName_txt.getText().trim().equals("")){
+						// return;
+						// }
 						JFileChooser fileChooser = new JFileChooser();
 						fileChooser.setDialogTitle("Choose a directory");
-						//fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+						// fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 						fileChooser.setVisible(true);
 						fileChooser.setSelectedFile(new File(fileToSave));
 						int returnVal = fileChooser.showSaveDialog(null);
-					    if(returnVal == JFileChooser.APPROVE_OPTION) {
-					    	savePath = fileChooser.getSelectedFile().getAbsolutePath();
-					    	parent.sendMessage(fileToSave + "\n" +savePath, CommandType.DownloadFileRequest);
-					    	dispose();
-					    	System.out.println(savePath);
-					    }
-					}
-				});	
-		
-		cancel.addActionListener(
-				new ActionListener(){
-					public void actionPerformed(ActionEvent e){
-						dispose();
+						if (returnVal == JFileChooser.APPROVE_OPTION) {
+							savePath = fileChooser.getSelectedFile().getAbsolutePath();
+							if (fileToSave.equals(fileChooser.getSelectedFile().getName())) {
+								JOptionPane.showMessageDialog(null, "Already exist in selected directory!");
+							} else {
+								parent.sendMessage(fileToSave + "\n" + savePath, CommandType.DownloadFileRequest);
+								dispose();
+							}
+						
+							// System.out.println(savePath);
+						}
 					}
 				});
+
+		cancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
 		tree.addTreeSelectionListener(new treeSelectionListener());
 	}
-	public void reloadFileList(String[] fileList)
-	{
-		
+	public void reloadFileList(String[] fileList) {
+
 		for (int i = 0; i < fileList.length; i++) {
 			top.add(new DefaultMutableTreeNode(fileList[i]));
 		}
 		tree.expandRow(0);
-		
+
 	}
-	class treeSelectionListener implements TreeSelectionListener
-	{
+
+	class treeSelectionListener implements TreeSelectionListener {
 
 		public void valueChanged(TreeSelectionEvent e) {
 			// TODO Auto-generated method stub
-			selectedNode = (DefaultMutableTreeNode)
-                    tree.getLastSelectedPathComponent();
+			selectedNode = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
 			fileToSave = selectedNode.getUserObject().toString();
-			
+
 		}
-		
+
 	}
 }
